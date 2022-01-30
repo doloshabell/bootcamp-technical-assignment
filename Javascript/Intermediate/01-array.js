02776f9b633637c6f022f2ddd0e40a7f5dc7822e
Habell Dolosha Purba <55502071+doloshabell@users.noreply.github.com>
2022-01-31 02:10:12 +0800

    update soal-01

--- Javascript/Intermediate/01-array.js
+++ Javascript/Intermediate/01-array.js
@@ -1,37 +1,37 @@
 //Soal - 01
-// const arr = [1, 2, 3];
-// const arr2 = [8, 1, 5, 7];
+const arr = [1, 2, 3];
+const arr2 = [8, 1, 5, 7];
 
-// function reverse(numb) {
-//     return [...numb].reverse();   
-// }
-// const newArr = reverse(arr);
-// const newArr2 = reverse(arr2);
+function reverse(numb) {
+    return [...numb].reverse();   
+}
+const newArr = reverse(arr);
+const newArr2 = reverse(arr2);
 
-// console.log(arr, newArr);
-// console.log(arr2, newArr2);
+console.log(arr, newArr);
+console.log(arr2, newArr2);
 
 /*Expected Result Soal - 01
 [1, 2, 3], [3, 2, 1]
 [8, 1, 5, 7], [7, 5, 1, 8] */
 
 //Soal - 02
-// const arr1 = [1, 3, 4, 1, 2, 8];
-// const arr2 = [5, 6, 7, 8, 1, 3];
+const arr1 = [1, 3, 4, 1, 2, 8];
+const arr2 = [5, 6, 7, 8, 1, 3];
 
-// function getAverage(arr) {
-//     const arrayAverage = arr.reduce((previousValue, currentValue) => previousValue + currentValue) / arr.length;
-//     const sumNumber = [];
-//     arr.forEach(num => {
-//         if (num>arrayAverage){
-//             sumNumber.push(num);
-//         }
-//     });
-//     return sumNumber.length;
-// }
+function getAverage(arr) {
+    const arrayAverage = arr.reduce((previousValue, currentValue) => previousValue + currentValue) / arr.length;
+    const sumNumber = [];
+    arr.forEach(num => {
+        if (num>arrayAverage){
+            sumNumber.push(num);
+        }
+    });
+    return sumNumber.length;
+}
 
-// console.log(getAverage(arr1));
-// console.log(getAverage(arr2));
+console.log(getAverage(arr1));
+console.log(getAverage(arr2));
 
 /*Expected Result Soal - 02
 2 // Ada 2 bilangan yang melebihi nilai rata-rata yaitu 4 & 8
