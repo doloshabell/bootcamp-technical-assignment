d426e259aefe00b91dc6a4206b3d749b1e5602bf
doloshabell <habelldol@gmail.com>
2022-03-16 10:12:24 +0700

    update create tables

--- mysql/mysql-lanjutan/skilvul_book_store.sql
+++ mysql/mysql-lanjutan/skilvul_book_store.sql
@@ -2,14 +2,26 @@ CREATE DATABASE skilvulbookstore;
 
 USE skilvulbookstore;
 
-SHOW TABLES;
-
 CREATE TABLE penerbit (
 	id INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     nama VARCHAR(50),
     kota VARCHAR(50)
 );
 
-SELECT * FROM penerbit;
+CREATE TABLE penulis (
+	id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nama VARCHAR(50),
+    kota VARCHAR(50)
+);
 
-desc penerbit;
+CREATE TABLE buku (
+	id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    isbn VARCHAR(50),
+    judul VARCHAR(50),
+    penulis VARCHAR(50),
+    penerbit INTEGER,
+    harga INTEGER,
+    stock INTEGER
+);
+
+SHOW TABLES;
