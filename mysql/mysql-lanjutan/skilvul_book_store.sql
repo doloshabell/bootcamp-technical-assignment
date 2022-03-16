4670b683d2310fc4972e41fa2952ebd567447c45
doloshabell <habelldol@gmail.com>
2022-03-16 10:10:59 +0700

    update create tables

--- mysql/mysql-lanjutan/skilvul_book_store.sql
+++ mysql/mysql-lanjutan/skilvul_book_store.sql
@@ -1,3 +1,15 @@
 CREATE DATABASE skilvulbookstore;
 
 USE skilvulbookstore;
+
+SHOW TABLES;
+
+CREATE TABLE penerbit (
+	id INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nama VARCHAR(50),
+    kota VARCHAR(50)
+);
+
+SELECT * FROM penerbit;
+
+desc penerbit;
