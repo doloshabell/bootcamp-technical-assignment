dec2e6a76476bde6a70047565b1b61a7e78a2663
doloshabell <habelldol@gmail.com>
2022-03-27 14:53:59 +0700

    update db.js and index.js

--- build-web-service-and-restful-api-with-expressjs-mongoose/config/db.js
+++ build-web-service-and-restful-api-with-expressjs-mongoose/config/db.js
@@ -1,8 +1,6 @@
 const mongoose = require("mongoose");
 
-const dbName = "highHaku"
-
-const uri = `mongodb+srv://doloshab:doloshab123@cluster0.ndftz.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+const uri = "mongodb+srv://doloshab:doloshab123@cluster0.ndftz.mongodb.net/highHaku?retryWrites=true&w=majority";
 
 const db = mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
 
--- build-web-service-and-restful-api-with-expressjs-mongoose/index.js
+++ build-web-service-and-restful-api-with-expressjs-mongoose/index.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const cors = require("cors");
+
 const db = require("./config/db");
 const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const PORT = process.env.PORT || 5000;
 
 db.then(() => console.log("Success connect to MongoDB Atlas"))
-    .catch(err => console.error(err));
+.catch(err => console.error(err));
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(PORT, () => {
     console.log("Server running on PORT", PORT);
