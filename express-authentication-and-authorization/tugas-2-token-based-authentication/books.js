c1b975df8b4fa5f2ef08994f50060e09b987f06d
doloshabell <habelldol@gmail.com>
2022-04-01 20:57:49 +0700

    update file books.js and folder tailwind

--- express-authentication-and-authorization/tugas-2-token-based-authentication/books.js
+++ express-authentication-and-authorization/tugas-2-token-based-authentication/books.js
@@ -5,7 +5,7 @@ const jwt = require("jsonwebtoken");
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-app.use(bodyParser.json());
+app.use(express.json());
 
 const books = [
     {
